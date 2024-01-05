caec3cbed8de274c24d073c14e7954e22a566cf7
The_UltimateNuke <84410206+TheUltimateNuke@users.noreply.github.com>
2024-01-04 21:11:37 -0500

    started working on new hooking patch

---
+++ src/Patches/Patch_MMHook.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace CementTools.Patches
+{
+    public static class Patch_MMHook
+    {
+        internal static void PatchHooks()
+        {
+            Assembly assembly = Assembly.LoadFile(Path.Combine("..", "MMHOOK", "MMHOOK_Assembly-CSharp.dll"));
+            foreach (Type typ in assembly.GetTypes())
+            {
+                foreach (EventInfo even in typ.GetEvents(AccessTools.all))
+                {
+                    Harmony harmony = new Harmony("cmt-mmhook-patch-harmony");
+
+                    harmony.Patch(even.RaiseMethod, new HarmonyMethod(typeof(Patch_MMHook).GetMethod(nameof(BeforeHook))));
+                }
+            }
+        }
+
+        public static void BeforeHook()
+        {
+            bool isCementMod = Path.GetFullPath(Cement.MODS_FOLDER_PATH) == Path.GetFullPath(Path.Combine(Assembly.GetCallingAssembly().Location, ".."))
+            if (isCementMod)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
